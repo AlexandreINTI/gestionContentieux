65e03bb905fe13678d54f68c59e5019e565a48d5
AlexandreINTI <al.poteau@outlook.fr>
2019-05-13 10:23:38 +0200

    class entity

---
+++ src/main/java/com/adaming/entity/Tribunal.java
@@ -0,0 +1,15 @@
+package com.adaming.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Tribunal implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/adaming/entity/Utilisateur.java
@@ -0,0 +1,26 @@
+package com.adaming.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Utilisateur implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idUtilisateur;
+	private String email;
+	private String prenom;
+	private String nom;
+	
+
+}
