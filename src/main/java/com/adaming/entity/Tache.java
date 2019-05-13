ca7757ceacc959a1e21a150a00e33529e41dc426
AlexandreINTI <al.poteau@outlook.fr>
2019-05-13 11:01:00 +0200

    Class role utili tache phase

--- src/main/java/com/adaming/entity/Tache.java
+++ src/main/java/com/adaming/entity/Tache.java
@@ -1,8 +1,15 @@
 package com.adaming.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Tache implements Serializable{
@@ -11,5 +18,129 @@ public class Tache implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Long idTache;
+	private Date dateCreation;
+	private String titre;
+	private String description;
+	private boolean statusAudience;
+	@OneToMany(mappedBy="tache")
+	private List<Phase> listphase=new ArrayList<Phase>();
+	@ManyToOne
+	private Affaire affaire;
+	@ManyToOne
+	private Tribunal tribunal;
+	
+	public Tache() {
+		super();
+	}
+
+	public Tache(Long idTache, Date dateCreation, String titre, String description, boolean statusAudience,
+			List<Phase> listphase, Affaire affaire, Tribunal tribunal) {
+		super();
+		this.idTache = idTache;
+		this.dateCreation = dateCreation;
+		this.titre = titre;
+		this.description = description;
+		this.statusAudience = statusAudience;
+		this.listphase = listphase;
+		this.affaire = affaire;
+		this.tribunal = tribunal;
+	}
+
+	public Tache(Date dateCreation, String titre, String description, boolean statusAudience, List<Phase> listphase,
+			Affaire affaire, Tribunal tribunal) {
+		super();
+		this.dateCreation = dateCreation;
+		this.titre = titre;
+		this.description = description;
+		this.statusAudience = statusAudience;
+		this.listphase = listphase;
+		this.affaire = affaire;
+		this.tribunal = tribunal;
+	}
+
+	public Tache(Date dateCreation, String titre, String description, boolean statusAudience) {
+		super();
+		this.dateCreation = dateCreation;
+		this.titre = titre;
+		this.description = description;
+		this.statusAudience = statusAudience;
+	}
+
+	public Long getIdTache() {
+		return idTache;
+	}
+
+	public void setIdTache(Long idTache) {
+		this.idTache = idTache;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isStatusAudience() {
+		return statusAudience;
+	}
+
+	public void setStatusAudience(boolean statusAudience) {
+		this.statusAudience = statusAudience;
+	}
+
+	public List<Phase> getListphase() {
+		return listphase;
+	}
+
+	public void setListphase(List<Phase> listphase) {
+		this.listphase = listphase;
+	}
+
+	public Affaire getAffaire() {
+		return affaire;
+	}
+
+	public void setAffaire(Affaire affaire) {
+		this.affaire = affaire;
+	}
+
+	public Tribunal getTribunal() {
+		return tribunal;
+	}
+
+	public void setTribunal(Tribunal tribunal) {
+		this.tribunal = tribunal;
+	}
+
+	@Override
+	public String toString() {
+		return "Tache [idTache=" + idTache + ", dateCreation=" + dateCreation + ", titre=" + titre + ", description="
+				+ description + ", statusAudience=" + statusAudience + "]";
+	}
+	
+	
+	
 
 }
--- src/main/java/com/adaming/entity/Utilisateur.java
+++ src/main/java/com/adaming/entity/Utilisateur.java
@@ -1,11 +1,17 @@
 package com.adaming.entity;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Utilisateur implements Serializable{
@@ -21,6 +27,109 @@ public class Utilisateur implements Serializable{
 	private String email;
 	private String prenom;
 	private String nom;
+	@ManyToMany(fetch=FetchType.EAGER)
+	@JoinTable(name="Utilisateur_Tache",
+			joinColumns= {@JoinColumn(name="idUtilisateur",referencedColumnName="idUtilisateur")},
+				inverseJoinColumns= {@JoinColumn(name="IdTache",table="tache",referencedColumnName="idTache")})
+	private Set<Tache> listtache=new HashSet<Tache>();
+	@ManyToMany
+	@JoinTable(name="Utilisateur_Role",
+			joinColumns= {@JoinColumn(name="idUtilisateur",referencedColumnName="idUtilisateur")},
+				inverseJoinColumns= {@JoinColumn(name="idRole",table="role",referencedColumnName="idRole")})
+	private Set<Role> listrole=new HashSet<Role>();
+
+	public Utilisateur() {
+		super();
+	}
+
+	public Utilisateur(Long idUtilisateur, String email, String prenom, String nom, Set<Tache> listtache) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.email = email;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.listtache = listtache;
+	}
+
+	public Utilisateur(Long idUtilisateur, String email, String prenom, String nom, Set<Tache> listtache,
+			Set<Role> listrole) {
+		super();
+		this.idUtilisateur = idUtilisateur;
+		this.email = email;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.listtache = listtache;
+		this.listrole = listrole;
+	}
+
+	public Utilisateur(String email, String prenom, String nom, Set<Tache> listtache) {
+		super();
+		this.email = email;
+		this.prenom = prenom;
+		this.nom = nom;
+		this.listtache = listtache;
+	}
+
+	public Utilisateur(String email, String prenom, String nom) {
+		super();
+		this.email = email;
+		this.prenom = prenom;
+		this.nom = nom;
+	}
+
+	public Long getIdUtilisateur() {
+		return idUtilisateur;
+	}
+
+	public void setIdUtilisateur(Long idUtilisateur) {
+		this.idUtilisateur = idUtilisateur;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Set<Tache> getListtache() {
+		return listtache;
+	}
+
+	public void setListtache(Set<Tache> listtache) {
+		this.listtache = listtache;
+	}
+
+	public Set<Role> getListrole() {
+		return listrole;
+	}
+
+	public void setListrole(Set<Role> listrole) {
+		this.listrole = listrole;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [idUtilisateur=" + idUtilisateur + ", email=" + email + ", prenom=" + prenom + ", nom="
+				+ nom + "]";
+	}
 	
 
 }
